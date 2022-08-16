77163338e88ab55728234b9c6b7e46036c68d48f
Vasyl Hrytsyshyn <matsjo19@gmail.com>
2022-08-16 23:16:21 +0300

    complete hw5

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,12 @@
 const validation = require("./validation")
 const isValidId = require("./isValidId")
 const auth = require("./auth")
+const upload = require("./upload")
 
 module.exports = {
     validation,
     isValidId,
     auth,
+    upload
+
 }
